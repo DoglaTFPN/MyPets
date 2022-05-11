0e02da15e3c67e102fd7af4a8ba8b34ca1530feb
Laboratorio-Info <douglas_saragoza@hotmail.com>
2022-05-11 20:17:45 -0300

    commit2

---
+++ app/src/main/java/br/com/etecia/mypets/CafeBadilico_activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.mypets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CafeBadilico_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cafe_badio_layout);
+    }
+}
